20cf84f4f85dc97223903c8b0af5d549dec1d233
kongxiangyu <kongxiangyu@feinno.com>
2019-04-12 16:07:42 +0800

    第一次提交测试

---
+++ gitForTest/gitForTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitForTest
+//
+//  Created by 孔祥宇 on 2019/4/12.
+//  Copyright © 2019 孔祥宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitForTest/gitForTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitForTest
+//
+//  Created by 孔祥宇 on 2019/4/12.
+//  Copyright © 2019 孔祥宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
